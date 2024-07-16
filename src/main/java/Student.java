2453a09a7dd787c073f2a1843ab56e8ea678e926
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-16 13:36:22 +0530

    Student Class

---
+++ src/main/java/Student.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    private String address;
+}
